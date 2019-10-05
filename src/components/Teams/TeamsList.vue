6d57e8a33be66d7a1bbf91244c86c09f7905908c
Adriano Costa <adrianokosta001@gmail.com>
2019-10-05 19:01:36 -0300

    Create TeamsList component

---
+++ src/components/Teams/TeamsList.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-row :gutter="20" class="teams__list">
+    <el-col
+      v-for="team in teams"
+      class="team__card-wrapper"
+      :key="team.Key"
+      :xs="20"
+      :sm="10"
+      :md="8"
+      :lg="6"
+      :xl="4"
+    >
+      <TeamCard :team="team"/>
+    </el-col>
+  </el-row>
+</template>
+
+<style>
+ .teams__list {
+    padding: 50px;
+ }
+
+ .team__card-wrapper {
+    padding: 10px;
+ }
+</style>
+
+<script>
+  import TeamCard from './TeamCard'
+
+  export default {
+    components: {
+      TeamCard
+    },
+    props:{
+      teams:{
+        type: Array,
+        default: () => []
+      }
+    }
+  }
+</script>
---
+++ src/components/Teams/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <body>
+    <Teamlist v-loading="loading" :teams.sync="teams"/>
+  </body>
+</template>
+
+<script>
+  import Teamlist from './TeamsList'
+  import * as TeamRepository from '../../repositories/TeamRepository'
+
+  export default {
+    data: () => ({
+      loading: true,
+      teams: []
+    }),
+    created() {
+      this.fetchTeams();
+    },
+    methods: {
+      async fetchTeams() {
+        this.teams = await TeamRepository.getAllTeams();
+        this.loading = false;
+      },
+    },
+    components:{
+      Teamlist
+    }
+  }
+</script>
+
